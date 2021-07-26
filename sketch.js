1d42345dcaf50a6cd7a844c50e68410412a3d331
DivyanshKawatra17 <79521502+DivyanshKawatra17@users.noreply.github.com>
2021-07-26 21:07:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var snowflakeImg
+var bgImg
+var snowflake
+function preload(){
+snowflakeImg = loadImage('snow4.webp');
+bgImg = loadImage('snow3.jpg');
+}
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+
+}
+
+function draw() {
+  background(bgImg);  
+  drawSprites();
+  createSnowflake();
+}
+function createSnowflake(){
+  if(frameCount%10===0){
+snowflake = createSprite(random(0,800),0,50,50)
+snowflake.velocityX = -2
+snowflake.velocityY = 4
+snowflake.addImage(snowflakeImg)
+snowflake.scale = 0.1
+  }
+}
